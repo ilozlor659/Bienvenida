a6620c5bdb1a800091f2d8d2f5fab225c7b013ef
estudiante <estudiante@oretania-PC>
2025-10-17 10:03:09 +0200

    Versión 1.5 Calculadora (Cambio de int a double y resto)

--- ProyectoCalculadora/src/ies_oretania/Calculadora.java
+++ ProyectoCalculadora/src/ies_oretania/Calculadora.java
@@ -7,8 +7,8 @@ public class Calculadora {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int num1;
-		int num2;
+		double num1;
+		double num2;
 		
 		System.out.println("Introduce un número: ");
 		num1 = sc.nextInt();
@@ -16,28 +16,31 @@ public class Calculadora {
 		num2 = sc.nextInt();
 		
 		//suma
-		int suma;
+		double suma;
 		suma = num1 + num2;
 		
 		System.out.println("La suma de los dos números es: " + suma);
 		
 		//resta
-		int resta;
+		double resta;
 		resta = num1 - num2;
 		
 		System.out.println("La resta de los dos números es: " + resta);
 		
 		//multiplicación
-		int multiplicacion;
+		double multiplicacion;
 		multiplicacion = num1 * num2;
 		
 		System.out.println("La multiplicación de los dos números es: " + multiplicacion);
 		
 		//división
-		int division;
+		double division;
 		division = num1 / num2;
+		double resto;
+		resto = num1 % num2;
 		
 		System.out.println("La división de los números es: " + division);
+		System.out.println("El resto de la división es: " + resto);
 		
 	
 
