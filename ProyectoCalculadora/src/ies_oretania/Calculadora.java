3fea7054b5e6d8da1c52137952fc1e3bf24ed15f
estudiante <estudiante@oretania-PC>
2025-10-17 10:12:10 +0200

    Versión 1.6 Calculadora (Retoques, comentarios y final)

--- ProyectoCalculadora/src/ies_oretania/Calculadora.java
+++ ProyectoCalculadora/src/ies_oretania/Calculadora.java
@@ -7,42 +7,46 @@ public class Calculadora {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		//Todo en double para permitir decimales
+		//variables
+		
 		double num1;
 		double num2;
 		
+		//Cambio de nextInt a nextDouble respecto a la versión 1.5
 		System.out.println("Introduce un número: ");
-		num1 = sc.nextInt();
+		num1 = sc.nextDouble();
 		System.out.println("Introduce un número: ");
-		num2 = sc.nextInt();
+		num2 = sc.nextDouble();
 		
 		//suma
 		double suma;
 		suma = num1 + num2;
 		
-		System.out.println("La suma de los dos números es: " + suma);
+		System.out.printf("La suma de los dos números es: %.2f \n", suma);
 		
 		//resta
 		double resta;
 		resta = num1 - num2;
 		
-		System.out.println("La resta de los dos números es: " + resta);
+		System.out.printf("La resta de los dos números es: %.2f \n ", resta);
 		
 		//multiplicación
 		double multiplicacion;
 		multiplicacion = num1 * num2;
 		
-		System.out.println("La multiplicación de los dos números es: " + multiplicacion);
+		System.out.printf("La multiplicación de los dos números es: %.2f \n", multiplicacion);
 		
-		//división
+		//división y resto
 		double division;
 		division = num1 / num2;
 		double resto;
 		resto = num1 % num2;
 		
-		System.out.println("La división de los números es: " + division);
-		System.out.println("El resto de la división es: " + resto);
+		System.out.printf("La división de los números es: %.2f \n", division);
+		System.out.printf("El resto de la división es: %.2f \n", resto);
 		
-	
+	//Código realizado por Isaac Lozano Lorente 1ºDAW
 
 	}
 
